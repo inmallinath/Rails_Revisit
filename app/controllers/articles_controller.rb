6e4e7986ae88e3a799fd0ebd98bd78d7e9b00484
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-10-10 17:15:35 -0700

    Implement update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,15 @@ class ArticlesController < ApplicationController
   def new
 
   end
+  def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -13,6 +22,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
   def show
     @article = Article.find(params[:id])
   end
